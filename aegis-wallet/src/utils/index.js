c6b88449ab3e714b8bbb7010bd4884147140c2ae
Lycurguz <amp@spear.technology>
2023-01-13 02:25:06 +1100

    big update
    included current working demo for aegis

--- aegis-wallet/src/utils/index.js
+++
@@ -1,9 +0,0 @@
-
-export function safeAccess (object, path) {
-  return object
-    ? path.reduce(
-      (accumulator, currentValue) => (accumulator && accumulator[currentValue] ? accumulator[currentValue] : null),
-      object
-    )
-    : null
-}
